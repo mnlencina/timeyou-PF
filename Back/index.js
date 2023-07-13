d8cd83f121e0a1ddaedbab934d929c7d3f63c69b
Jose Miguel <josemptj@gmail.com>
2023-07-13 11:03:16 -0300

    permisos CORS

--- Back/index.js
+++ Back/index.js
@@ -2,4 +2,4 @@ require("dotenv").config();
 const server = require("./src/App");
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => console.log(`Server on PORT ${PORT}`));
+server.listen(PORT, () => console.log(`server on PORT ${PORT}`));
