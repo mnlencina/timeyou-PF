c85c1527c3fcf390e8aee3add76415925a1427d6
milward larico tuni <1000ward23@gmail.com>
2023-07-13 11:21:14 -0500

    agregando Watch.js

---
+++ Back/src/models/watch.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Watch', {
+
+    id : {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4, // Or DataTypes.UUIDV1
+        primaryKey: true,
+    },
+    model: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    color: {
+        type: DataTypes.JSON,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    gender: {
+        type: DataTypes.ENUM('Male','Female','Unisex'),
+        allowNull: false,
+    },
+    reviews: {
+        type: DataTypes.ENUM('0','1','2','3','4','5'),
+        allowNull: true,
+    },
+    delete: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: true,
+    },
+  },{
+    timestamps: false, // Desactivar timestamps
+  });
+};
