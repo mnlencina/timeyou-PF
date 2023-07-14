3de98735cb58d68332eede80d6ab14acec5b4b53
Jose Miguel <josemptj@gmail.com>
2023-07-14 14:13:51 -0300

    back/src/routes-and-controllers creacion ruta y controller post de strap

--- Back/src/routes/index.js
+++ Back/src/routes/index.js
@@ -10,4 +10,15 @@ router.post("/admin", (req, res) => {
   res.status(200).send("aca se creara el nuevo items");
 });
 
+router.post("/admin/addStrap", async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const newStrap = await createStrap(name);
+    res.status(200).json(newStrap);
+  } catch (error) {
+    res.status(500).json({ Error: error.message });
+  }
+});
+
 module.exports = router;
