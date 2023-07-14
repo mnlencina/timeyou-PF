034b56ef417211a2ebb219da24089f9009b6fa33
Jose Miguel <josemptj@gmail.com>
2023-07-14 13:00:14 -0300

    back/src/routes-and-controllers creacion ruta y controller post de style

--- Back/src/routes/index.js
+++ Back/src/routes/index.js
@@ -10,4 +10,14 @@ router.post("/admin", (req, res) => {
   res.status(200).send("aca se creara el nuevo items");
 });
 
+router.post("/admin/addStyle", async (req, res) => {
+  const { name } = req.body;
+  try {
+    const newStyle = await createNewStyle(name);
+    res.status(200).json(newStyle);
+  } catch (error) {
+    res.status(500).json({ Error: error.message });
+  }
+});
+
 module.exports = router;
