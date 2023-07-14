44585288c75681e7f1c19e9a5c1189ada5494558
felipewyss <felipeewyss@gmail.com>
2023-07-14 11:33:19 -0300

    ruta para obtener colores para filtro

--- Back/src/controllers/getColors.js
+++ Back/src/controllers/getColors.js
@@ -1,6 +1,19 @@
-const getColors = (color) => {
+const { watch } = require("../db");
 
-    return getColors;
+
+const getColors = async (color) => {
+    const watches = await watch.findAll()
+    const colors= watches.map(w=>w.color)
+    const allFilterOptionsColor = [];
+    colors.map(c=> allFilterOptionsColor.push(...Object.keys(c)))
+    const filterOptionsColor = []
+    for (let index = 0; index < allFilterOptionsColor.length; index++) {
+        const element = array[index];
+        if(!filterOptionsColor.includes(element)){
+            filterOptionsColor.push(element);
+        }
+    }
+    return filterOptionsColor;
     };
     
     
