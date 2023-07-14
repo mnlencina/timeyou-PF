25d9ac685ca58d0e71e329f87031eb3698c76610
Jose Miguel <josemptj@gmail.com>
2023-07-13 21:19:45 -0300

    Back/src/controllers/createNewWatch

--- Back/src/controllers/createNewWacth.js
+++ Back/src/controllers/createNewWacth.js
@@ -1,6 +1,15 @@
-const { watch } = require("../db");
+const { watch, Function, brand, Style, Strap } = require("../db");
 
-const createNewWatch = async (model, color, price, gender) => {
+const createNewWatch = async (
+  model,
+  color,
+  price,
+  gender,
+  brands,
+  style,
+  strap,
+  functions
+) => {
   // evaluamos que recibimos los datos completos para crear el nuevo reloj
   if (!model || !color || price || gender) throw new Error("missing data");
 
@@ -10,8 +19,23 @@ const createNewWatch = async (model, color, price, gender) => {
     price,
     gender,
   });
+
+  const brandModel = await brand.findOne({ where: { name: brands } });
+
+  const styleModel = await Style.findOne({ where: { name: style } });
+
+  const strapModel = await Strap.findOne({ where: { name: strap } });
+
+  const functModels = await functions.map((funct) => {
+    return Function.findOne({ where: { name: funct } });
+  });
+
+  await newWatch.setFunctions(functModels);
+  await newWatch.setStyles(styleModel);
+  await newWatch.setBrands(brandModel);
+  await newWatch.setStraps(strapModel);
+
   return newWatch;
-  console.log("hola");
 };
 
 module.exports = createNewWatch;
