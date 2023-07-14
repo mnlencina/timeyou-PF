36c6670983ab9278fc7129f247b85c8905787be3
Jose Miguel <josemptj@gmail.com>
2023-07-13 21:51:25 -0300

    back/src/controllers/postNewWatch and  /index.js

--- Back/index.js
+++ Back/index.js
@@ -1,11 +1,10 @@
-
 const server = require("./src/App");
-const { conn } = require('./src/db.js');
+const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
 //server.listen(PORT, () => console.log(`server on PORT ${PORT}`));
 conn.sync({ alter: false }).then(() => {
-    server.listen(PORT, () => {
-      console.log(`server on PORT ${PORT}`);
-    });
+  server.listen(PORT, () => {
+    console.log(`server on PORT ${PORT}`);
   });
+});
