b29b4d60e14b112e03e5235aecd5ab7f4cfeb454
milward larico tuni <1000ward23@gmail.com>
2023-07-17 11:46:52 -0500

    actualizando

--- Back/src/controllers/postFunction.js
+++ Back/src/controllers/postFunction.js
@@ -1,14 +1,8 @@
 const { Function } = require("../db");
 
-const createNewFunction = (functions) => {
-  if (functions.length == 0) throw new Error("Missing data");
-  const newFuntions = functions.map(e => {
-    return {
+const createFunction = async (functionName) => {
+  const newFunction= await Function.create({ name: functionName })
+  return newFunction;
+  };
 
-      name:e
-    }
-  });
-  return Function.bulkCreate(newFuntions);
-};
-
-module.exports = createNewFunction;
+module.exports = createFunction;
