797bb48c2ffe1300df4ad3218ad5f105201d5490
Magaerv <marielaramirezvalle@gmail.com>
2023-07-19 11:16:37 -0300

    ruta-get-allstyles

--- Back/src/routes/style/styleRouter.js
+++ Back/src/routes/style/styleRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const getStyles = require("../../controllers/getStyles");
 const createStyle = require("../../controllers/postStyle");
+const { getAllStyles, getStyles } = require("../../controllers/getStyles");
 const styleRouter = express.Router();
 
 styleRouter.post("/", async (req, res) => {
@@ -22,5 +22,14 @@ styleRouter.get("/:styleName", async (req, res) => {
     res.status(500).json({ Error: error.message });
   }
 });
+
+styleRouter.get("/", async (req, res) => { 
+  try {
+    const allStyles = await getAllStyles();
+    res.status(200).json(allStyles);
+  } catch (error) {
+    res.status(500).json({ Error: error.message });
+  }
+})
 
 module.exports = styleRouter;
