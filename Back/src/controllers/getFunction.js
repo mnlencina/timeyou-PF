85c5ac3c0ad1ef6ab32dd5154c91864eb8e632ad
mnlencina <mnlencina@gmail.com>
2023-07-19 08:58:56 -0300

    ccorreccion getFunction

--- Back/src/controllers/getFunction.js
+++ Back/src/controllers/getFunction.js
@@ -4,7 +4,11 @@ const { Function, Watch} = require("../db");
 const getFunctions = async (functionName) => {
     const functions= await Function.findOne({
         where: { name: functionName },
-        include: Watch // Incluir la relación de los relojes asociados al color
+        include: [
+          {
+            model: Watch
+          }
+        ] // Incluir la relación de los relojes asociados al color
       });
     return functions;
     };
