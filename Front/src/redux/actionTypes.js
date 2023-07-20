6719313a0a309479a7b50b8a47ee9a4c54ae2e8e
CVazquezOpalka <cristianvazquez80@gmail.com>
2023-07-20 15:44:53 -0300

    creando detailLoading

--- Front/src/redux/actionTypes.js
+++ Front/src/redux/actionTypes.js
@@ -1,2 +1,8 @@
 export const GET_PRODUCTS = "GET_PRODUCTS";
 export const GET_PRODUCTS_DETAIL = "GET_PRODUCTS_DETAIL";
+
+//funciones de carrito de compras
+
+export const ADD_TO_CART = "ADD_TO_CART";
+export const CLEAR_CART = "CLEAR_CART";
+export const CLEAR_ONE_PRODUCT = "CLEAR_ONE_PRODUCT";
