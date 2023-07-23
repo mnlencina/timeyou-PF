f2f395a24edebaf3038e903685ce2f191102f271
CVazquezOpalka <cristianvazquez80@gmail.com>
2023-07-23 03:22:12 -0300

    cambiando cosas

--- Front/src/redux/Actions.js
+++ Front/src/redux/Actions.js
@@ -57,7 +57,7 @@ export const addToCart = (product) => ({
   type: ADD_TO_CART,
   payload: product,
 });
-
+ 
 export const removeFromCart = (productId) => ({
   type: REMOVE_FROM_CART,
   payload: productId,
