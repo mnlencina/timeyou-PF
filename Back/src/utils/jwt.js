7dc8450c2cc9765ea959fadf3a21bc0f7a621a96
Jose Miguel <josemptj@gmail.com>
2023-07-24 17:55:11 -0300

    require functions OK

--- Back/src/utils/jwt.js
+++ Back/src/utils/jwt.js
@@ -7,7 +7,7 @@ const createAccessToken = (payload) => {
   return new Promise((resolve, reject) => {
     jwt.sign(
       payload,
-      SECRET_TOKEN, //debe estar en un archivo que no tenga acceso HACKER
+      SECRET_TOKEN, //debe estar en un archivo que no tenga acceso HACKER esto tendran que crealo en su local para poder usar la ruta.
       {
         expiresIn: "1d", //tiempo en el que expra el token
       },
