894e6a231044f2167e24d63ee3403b951fa2f490
Jose Miguel <josemptj@gmail.com>
2023-07-27 10:49:09 -0300

    ruta GetUserId para admin

--- Back/src/routes/admin/adminRouter.js
+++ Back/src/routes/admin/adminRouter.js
@@ -14,17 +14,6 @@ adminRouter.get("/allUsers", async (req, res) => {
   }
 });
 
-adminRouter.put("/inabilited/:id", async (req, res) => {
-  const { id } = req.params;
-  const { del } = req.body;
-  try {
-    const inabilited = await inabilitedUser(id, del);
-    res.status(200).json(inabilited);
-  } catch (error) {
-    res.status(500).json({ Error: error.message });
-  }
-});
-
 adminRouter.get("/users-by-email", async (req, res) => {
   const { email } = req.query;
   try {
@@ -34,6 +23,29 @@ adminRouter.get("/users-by-email", async (req, res) => {
     if (error.message.includes("Email"))
       return res.status(404).json({ Error: error.message });
     else return res.status(500).json({ Error: error.message });
+  }
+});
+
+adminRouter.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userByIdentifier = await userById(id);
+    res.status(200).json(userByIdentifier);
+  } catch (error) {
+    if (error.message.includes("uuid"))
+      return res.status(404).json({ Error: error.message });
+    else return res.status(500).json({ Error: error.message });
+  }
+});
+
+adminRouter.put("/inabilited/:id", async (req, res) => {
+  const { id } = req.params;
+  const { del } = req.body;
+  try {
+    const inabilited = await inabilitedUser(id, del);
+    res.status(200).json(inabilited);
+  } catch (error) {
+    res.status(500).json({ Error: error.message });
   }
 });
 module.exports = adminRouter;
