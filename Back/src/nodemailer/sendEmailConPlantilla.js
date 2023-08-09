a16017c30027e734be213eb9e2f0c4c70480bf90
JuanManuelA12 <aguirre.juanmanuel029@gmail.com>
2023-08-08 20:20:10 -0400

    nodemailercorregido

--- Back/src/nodemailer/sendEmailConPlantilla.js
+++ Back/src/nodemailer/sendEmailConPlantilla.js
@@ -7,13 +7,12 @@ let emailOptions;
     switch(template) {
         case "newUser": 
          emailOptions = {
-            from: "timeYou",
+            from: "pagetimeyou@gmail.com",
             to,
             subject: "Bienvenido a timeYou",
             html: 
-            "<h1> Bienvenido a timeYou </h1>. <p> ¡Bienvenido/a a nuestra tienda en línea de relojes! Nos complace que nos hayas encontrado entre la gran cantidad de opciones que existen en internet. Aquí encontrarás una amplia variedad de relojes de calidad, desde los más clásicos y elegantes hasta los más modernos y deportivos. En nuestra tienda nos esforzamos por ofrecerte la mejor experiencia de compra posible, por lo que hemos creado un sitio web fácil de navegar y con toda la información que necesitas para tomar la mejor decisión al elegir tu reloj. Además, nuestro equipo de atención al cliente está a tu disposición para resolver cualquier duda o inquietud que puedas tener"
-          
-          };
+          `<h1> Bienvenido a timeYou </h1>.<p>¡Bienvenido/a a nuestra tienda en línea de relojes! Nos complace que nos hayas encontrado entre la gran cantidad de opciones que existen en internet. Aquí encontrarás una amplia variedad de relojes de calidad, desde los más clásicos y elegantes hasta los más modernos y deportivos. En nuestra tienda nos esforzamos por ofrecerte la mejor experiencia de compra posible, por lo que hemos creado un sitio web fácil de navegar y con toda la información que necesitas para tomar la mejor decisión al elegir tu reloj. Además, nuestro equipo de atención al cliente está a tu disposición para resolver cualquier duda o inquietud que puedas tener.`         
+        };
           break; 
           default:
             emailOptions = {
